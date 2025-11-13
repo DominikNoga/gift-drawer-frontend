f04296827fc38880227019948615305e91e9ec40
DominikNoga <dominik.noga2001@gmail.com>
2025-11-13 16:55:15 +0100

    hotfix

--- src/pages/EventPage/components/MainSection/components/TabView/components/ParticipantsTab/ParticipantsTab.tsx
+++ src/pages/EventPage/components/MainSection/components/TabView/components/ParticipantsTab/ParticipantsTab.tsx
@@ -15,7 +15,7 @@ export default function ParticipantsTab({ participants }: Props) {
       {participants?.map((participant) => (
         <ParticipantsTabItem
           key={participant.name}
-          name={'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'}
+          name={participant.name}
           joinCode={participant.joinCode}
         />
       ))}
